ff8e612298fdac6d9b1c61488d85ad91a305d5d7
KrisTovski <kfilak@onet.eu>
2022-02-15 13:32:25 +0100

    ref

--- src/com/kristovski/Game.java
+++ src/com/kristovski/Game.java
@@ -4,27 +4,27 @@ import com.kristovski.model.Board;
 import com.kristovski.model.Point;
 import com.kristovski.model.Ship;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import static com.kristovski.utils.Constants.*;
 
 public class Game {
 
-    Scanner scanner = new Scanner(System.in);
-    Board board = new Board(BOARD_SIZE);
+    private Board board = new Board(BOARD_SIZE);
+    private List<Ship> shipList = new ArrayList<>();
 
     Ship battleship = new Ship(5, null, new Random().nextBoolean(), false);
     Ship destroyer1 = new Ship(4, null, new Random().nextBoolean(), false);
     Ship destroyer2 = new Ship(4, null, new Random().nextBoolean(), false);
-
-    public List<Ship> shipList = new ArrayList<>();
 
     public void start() {
         initializeBoardGame();
         addShipToList();
         insertShip();
         printCurrentStateOfTheBoard();
-        printCurrentStateOfTheBoardWithShipsHidden();
         play();
     }
 
@@ -32,31 +32,72 @@ public class Game {
         shipList.add(battleship);
         shipList.add(destroyer1);
         shipList.add(destroyer2);
-
     }
 
+    public void play() {
+        do {
+            String coordinates = readCoordinatesFromUser();
+
+            int x = getXFromLetterCoordinate(coordinates);
+            int y = getYFromNumberCoordinate(coordinates);
+
+            char[][] matrix = board.getBoard();
+
+            if (matrix[y][x] == SHIP) {
+                board.getBoard()[y][x] = HIT;
+                System.out.println("HIT");
+
+            } else if (matrix[y][x] == WATER) {
+                board.getBoard()[y][x] = MISSED;
+                System.out.println("MISSED");
+            }
+            checkIfShipSunk();
+            printCurrentStateOfTheBoard();
+
+        } while (!isAllShipSunk());
+
+        if (isAllShipSunk()) {
+            System.out.println("CONGRATULATIONS, YOU WIN!");
+        }
+    }
+
+    private String readCoordinatesFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        String coordinates = "";
+        String pattern = "^[a-jA-J](?:[1-9]|10)$";
+        while (!coordinates.matches(pattern)) {
+            System.out.println("Choose coordinates, e.g. A1, C4, G8, etc: ");
+            coordinates = scanner.nextLine();
+            if (!coordinates.matches(pattern)) {
+                System.out.println("Wrong coordinates: " + coordinates + "\nTry again!");
+            }
+        }
+        return coordinates;
+    }
+
+
     public void initializeBoardGame() {
-        printColumnLetters(board);
-
-        int startRowNumber = START_ROW_NUMBER;
-
         for (int i = 0; i < board.getSize(); i++) {
-
-            startRowNumber = printRowNumber(board, startRowNumber, i);
-
             for (int j = 0; j < board.getSize(); j++) {
                 char[][] matrix = board.getBoard();
                 matrix[i][j] = WATER;
-
-                System.out.print(matrix[i][j] + "  ");
             }
-            System.out.println();
         }
-
     }
 
-    //Helper method to show ships on the board
-    public void printCurrentStateOfTheBoardWithShipsHidden() {
+    public void insertShip() {
+        while (!isAllShipsHaveStartPoints()) {
+            for (Ship ship : shipList) {
+                if (ship.getStartPoint() == null) {
+                    setShipRandomCoordinates(ship);
+                    Point startPoint = ship.getStartPoint();
+                    addShipToBoard(ship, startPoint);
+                }
+            }
+        }
+    }
+
+    public void printCurrentStateOfTheBoard() {
         printColumnLetters(board);
         int startRowNumber = START_ROW_NUMBER;
 
@@ -72,27 +113,8 @@ public class Game {
                     System.out.print(matrix[i][j] + "  ");
             }
             System.out.println();
-
         }
     }
-
-    public void printCurrentStateOfTheBoard() {
-        printColumnLetters(board);
-        int startRowNumber = START_ROW_NUMBER;
-
-        for (int i = 0; i < board.getSize(); i++) {
-
-            startRowNumber = printRowNumber(board, startRowNumber, i);
-
-            for (int j = 0; j < board.getSize(); j++) {
-                char[][] matrix = board.getBoard();
-                System.out.print(matrix[i][j] + "  ");
-            }
-            System.out.println();
-
-        }
-    }
-
 
     private int printRowNumber(Board board, int startRowNumber, int i) {
         if (i < board.getSize() - 1) {
@@ -117,43 +139,11 @@ public class Game {
         System.out.println();
     }
 
-
-    public void play() {
-
-        do {
-            System.out.println("Choose coordinates, e.g. A1, C4, G8, etc: ");
-            String coordinates = scanner.nextLine();
-            int x = getXFromLetterCoordinate(coordinates);
-            int y = getYFromNumberCoordinate(coordinates);
-
-            System.out.println(x + " " + y);
-
-            char[][] matrix = board.getBoard();
-
-            if (matrix[y][x] == SHIP) {
-                board.getBoard()[y][x] = HIT;
-                System.out.println("Hit");
-
-            } else if (matrix[y][x] == WATER) {
-                board.getBoard()[y][x] = MISSED;
-                System.out.println("missed");
-            }
-
-            checkIfShipSunk();
-            printCurrentStateOfTheBoard();
-
-        } while (!isAllShipSunk());
-        if(isAllShipSunk()){
-            System.out.println("CONGRATULATIONS, YOU WIN!");
-        }
-
-    }
-
     private boolean isAllShipSunk() {
         int counter = shipList.size();
         for (Ship ship : shipList) {
-            if(ship.isDestroyed()){
-            counter--;
+            if (ship.isDestroyed()) {
+                counter--;
             }
         }
         return counter == 0;
@@ -183,7 +173,7 @@ public class Game {
                     }
                 }
                 if (lives == 0 && !ship.isDestroyed()) {
-                   ship.setDestroyed(true);
+                    ship.setDestroyed(true);
                     System.out.println("SUNK");
 
                 }
@@ -213,21 +203,6 @@ public class Game {
         Point point = new Point(random.nextInt(board.getSize()), random.nextInt(board.getSize()));
         ship.setStartPoint(point);
 
-    }
-
-    public void insertShip() {
-
-        while (!isAllShipsHaveStartPoints()) {
-            for (Ship ship : shipList) {
-                if (ship.getStartPoint() == null) {
-                    setShipRandomCoordinates(ship);
-
-                    Point startPoint = ship.getStartPoint();
-
-                    addShipToBoard(ship, startPoint);
-                }
-            }
-        }
     }
 
     private void addShipToBoard(Ship ship, Point startPoint) {
@@ -283,7 +258,5 @@ public class Game {
             }
             return free;
         }
-
-
     }
 }
